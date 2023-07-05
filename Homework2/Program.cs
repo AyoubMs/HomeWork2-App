914b84abcadd74e3f1d0eecce783a8b2605ac09d
AyoubMS <chahayb563@gmail.com>
2023-07-05 13:05:01 +0200

    add app

---
+++ Homework2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+Console.Write("Please enter your age: ");
+
+string ageText = Console.ReadLine();
+
+bool isValid = int.TryParse(ageText, out int age);
+
+Console.WriteLine($"You would be {age + 25} in 25 years, and 25 years ago, you were {age - 25}");
